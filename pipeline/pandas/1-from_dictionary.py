5202bb74663784b98ae93ecfc2680cca060c3524
Jallah Sumbo <j.smbo@alustudent.com>
2025-02-18 13:22:33 +0000

    task 2

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,6 @@
+# Create a dictionary
+data = {'First': [0.0, 0.5, 1.0, 1.5],
+        'Second': ['one', 'two', 'three', 'four']}
+
+# Create a DataFrame
+df = pd.DataFrame(data, index=['A', 'B', 'C', 'D'])
